5fc8f0c9948c79c96204e3a77d70157b35843e14
Jim Ancona <jim@anconafamily.com>
2024-06-22 10:39:21 -0400

    Maine and NH DMR plus analog

--- input/default/generate.py
+++ input/default/generate.py
@@ -12,19 +12,19 @@ cp_dir = Path(__file__).parent
 output = Path(os.environ.get("OUTPUT") or (cp_dir / ".." / ".." / "OUTPUT"))
 
 CodeplugRecipe(
-    source_pnwdigital=True,
-    source_seattledmr=True,
-    source_default_k7abd=True,
+    # source_pnwdigital=True,
+    # source_seattledmr=True,
+    # source_default_k7abd=True,
     source_k7abd=[(cp_dir / "k7abd")],
     source_repeaterbook_proximity=cp_dir / "prox.csv",
-    repeaterbook_states=["washington", "oregon"],
-    repeaterbook_name_format='{Callsign} {Nearest City} {Landmark}',
-    scanlists_json=cp_dir / "scanlists.json",
-    exclude=cp_dir / "exclude.csv",
-    order=cp_dir / "order.csv",
-    replacements=cp_dir / "replacements.csv",
-    output_anytone=True,
-    output_dmrconfig=[(cp_dir / "example-d878uv.conf")],
-    output_farnsworth=[(cp_dir / "example-md-uv380.json")],
-    output_gb3gf=True
+    repeaterbook_states=["maine","new hampshire","vermont","new york"],
+    repeaterbook_name_format='{Nearest City} {Frequency} {Callsign}',
+    # scanlists_json=cp_dir / "scanlists.json",
+    # exclude=cp_dir / "exclude.csv",
+    # order=cp_dir / "order.csv",
+    # replacements=cp_dir / "replacements.csv",
+    output_anytone=[ "878_1_21" ],
+    output_dmrconfig=[(cp_dir / "dmr6x2-jim.conf")],
+    # output_farnsworth=[(cp_dir / "example-md-uv380.json")],
+    # output_gb3gf=True
 ).generate(output / cp_dir.name)
